3d14e1e27b2572648c254b4d9d43a2e1dbfbd67e
isac <isacszc@hotmail.com>
2025-06-08 18:16:26 -0300

    Criei a tela inicial

---
+++ src/comps/Login.js
@@ -0,0 +1,66 @@
+import { View, Text, TextInput, Button, StyleSheet, Alert } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import React, {useState} from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const stack = createNativeStackNavigator();
+
+function LoginScreen({ navigation}) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    // identificar login
+    const idfLogin = () => {
+        if(username ==='admin' && password === '123456') {
+            navigation.navigate('Home');
+        }else{
+            Alert.alert('Erro identificado!','Login ou senha incorretos');
+        }
+    };
+
+    return(
+        <View style={styles.container}>
+            <Text style={styles.titulo}>Login</Text>
+            <TextInput
+                style={styles.box}
+                placeholder="Usuário"
+                onChangeText={setUsername}
+                value={username}
+            />
+                <TextInput 
+                style={styles.box}
+                placeholder="Senha"
+                secureTextEntry
+                onChangeText={setPassword}
+                value="password"
+                />
+            <Button title="Entrar" onPress={idfLogin} />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: 16,
+    },
+    titulo: {
+        fontSize: 28,
+        marginBottom: 20,
+        textAlign: 'center',
+    },
+    box: {
+        height: 48,
+        borderWidth: 2,
+        borderColor: '#499',
+        borderRadius: 10,
+        marginBottom: 12,
+        paddingHorizontal: 12,
+        width: 550,
+        alignSelf: 'center'
+    },
+})
+
+
+export default LoginScreen
